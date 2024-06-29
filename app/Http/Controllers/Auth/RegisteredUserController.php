2de38d7b65da457a9a53b17fb000503772ebb530
afta <hattpri.yhukimura@gmail.com>
2024-06-29 23:14:46 +0700

    repairing login-register #1

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -30,14 +30,15 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'username' => ['required', 'string', 'max:255'],
+            // 'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
-
+        
+        // dd($request->all());
         $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
+            'username' => $request->username,
+            // 'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,23 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->bigInteger('emp_id')->unique()->nullable();
+            $table->string('username');
             $table->string('password');
             $table->rememberToken();
+            $table->boolean('active')->default(true);
+            $table->boolean('superadmin')->default(false);
+            $table->boolean('admin')->default(false);
+            $table->boolean('hrd')->default(false);
+            $table->boolean('hd')->default(false);
+            $table->boolean('producer')->default(false);
+            $table->boolean('pm')->default(false);
+            $table->boolean('coor')->default(false);
+            $table->boolean('spv')->default(false);
+            $table->boolean('officer')->default(false);
+            $table->boolean('production')->default(false);
+            $table->boolean('liveshoot')->default(false);
+            $table->tinyInteger('blocked')->default(0);
             $table->timestamps();
         });
 
